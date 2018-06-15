81f2c490060d936727167fd5cd6b0ec63bc0f80b
Yelson <yelsonmonge@gmail.com>
2018-06-14 22:06:26 -0600

    Pruebas

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-var http = require('http');
-var fs = require('fs');
-
-var server = http.createServer(function(req, res){
-	console.log('request was made: ' + req.url);
-
-	//Conectar al html
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	var myReadStream = fs.createReadStream(__dirname + '/principal.html', 'utf8');
-	myReadStream.pipe(res);
-	
-});
-function myFunction() {
-      document.getElementById("btn").value = "YOU CLICKED ME!";
- 	};
-server.listen(8080, '127.0.0.1');
-console.log('Yelson is running...');
+alert("HolaMundo!");
