bf2f21aca83d25718bfaf99a858f358041af03f4
Yelson <yelsonmonge@gmail.com>
2018-06-14 21:06:09 -0600

    Pruebas de enlace JS-HTML

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
 	console.log('request was made: ' + req.url);
+
+	//Conectar al html
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	var myReadStream = fs.createReadStream(__dirname + '/principal.html', 'utf8');
 	myReadStream.pipe(res);
+	
 });
-
-server.listen(3000, '127.0.0.1');
-console.log('Yelson');
+function myFunction() {
+      document.getElementById("btn").value = "YOU CLICKED ME!";
+ 	};
+server.listen(8080, '127.0.0.1');
+console.log('Yelson is running...');
