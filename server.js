4f95a58c01708c0587a86ee289db05b3ccf44953
Cali <carlosvega124@gmail.com>
2018-06-19 11:32:10 -0600

    Estructura Hash y algoritmo de busqueda

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require("http"),
- fs = require ("fs");
+fs = require ("fs");
  
   
 http.createServer(function (req, res){
@@ -23,42 +23,171 @@ http.createServer(function (req, res){
 	for (var i = variables.length - 1; i >= 0; i--) {
 	 	var variable = variables[i];
 	 	html_string = html_string.replace("{"+variables[i]+"}", parametros [variable]);
+	
 	}
-	getYear(parametros[variable]);
-	console.log(parametros[variable]);
+	
+	var yearsHash = createHash();
+	list = searchMovie(yearsHash , 2005, 2015, 'good, Tomorrowland', 'Animation, Drama', 'Neff');
+	
 	res.writeHead (200, {"Content-Type":"text/html"})
 	res.write(html_string);
 	res.end();
 	});
 
-//JSON
 	function ReadToArray (filename) {
     var data = JSON.parse(fs.readFileSync(filename));
-    //console.log(data);
-    return data;
+    	return data;
 	};
 
-	function loadFile(filepath){
-		var arrLines = [];
-		fs.stat(filepath, function(err, stat) {
-			if(err == null) {
-				arrLines = fsReadFileSynchToArray(filepath);
-			} else if(err.code == 'ENOENT') {
-				console.log('error: loading file ' + filepath + ' not found');
-			} else {
-				console.log('error: loading file', err.code);
-			}
-		});
-		return arrLines;
-	};
-
-	function getYear(pMovieName){
-		//Carga el JSON
+	function createHash(){
+		var yearsHash = new HashTable();
 		var jsonObject = ReadToArray("videosdb.json");
-		for(var movie in jsonObject){
-			if (pMovieName == jsonObject[movie].title)			
-			console.log(jsonObject[movie].title, jsonObject[movie].year);
+		for(var indexMovie in jsonObject){
+			if (jsonObject[indexMovie].genre != null){
+				var listGenre = commaSplit(jsonObject[indexMovie].genre);
+				for (var indexGenre in listGenre){
+					yearsHash.setItemYear(jsonObject[indexMovie].year, listGenre[indexGenre].toLowerCase(), jsonObject[indexMovie]);	
+				}				
+			}
+			else{
+				yearsHash.setItemYear(jsonObject[indexMovie].year, "null", jsonObject[indexMovie]);
+			}
 		}
+		return yearsHash;
+	}
+
+	function commaSplit(pString){
+		var listWords = [];
+		if (pString.indexOf(',') > 0){
+			listWords = pString.split(", ");
+		}
+		else{ 
+			listWords.push(pString);
+		}
+		return listWords;
+	}
+
+	function searchMovie(pYearsHash, pStart, pFinal, pName, pGenre, pCast){
+		var listMovieName = [];
+		var listMovieCast = [];
+	    for (var indexYear = pStart; indexYear <= pFinal; indexYear++) {
+            console.log(indexYear);
+            listGenre = commaSplit(pGenre);
+            for (var indexGenre in listGenre) { //itera sobre los generos de cada año
+            	genre = listGenre[indexGenre].toLowerCase();
+            	if (pYearsHash.items[indexYear].hasItem(genre)){
+            		var listMovieNode = pYearsHash.items[indexYear].items[genre];
+            		listMovieName = searchMovieName(pYearsHash.items[indexYear].items[genre], pName.toLowerCase(), listMovieName);
+            		listMovieCast = searchMovieCast(listMovieNode, pCast.toLowerCase(), listMovieCast);
+            	}
+	    	}
+	    }
+	    listMovie = intersectionMovies(listMovieName, listMovieCast);
+	    return listMovie;
+	}
+
+	function intersectionMovies(pListMovieName, pListMovieCast){
+		var listMovies = []; 
+		for (var indexMovie in pListMovieName) {
+			if (pListMovieCast.includes(pListMovieName[indexMovie])) {
+				listMovies.push(pListMovieName[indexMovie]);	
+			}
+		}
+		return listMovies;
+	}
+
+	function searchMovieName(pListMovies, pListMoviesName, lista){
+		for (var indexMovie in pListMovies) {
+			listMovieName = commaSplit(pListMoviesName);
+			for (indexMovieName in listMovieName)
+			{
+				movieName = pListMovies[indexMovie].title.toLowerCase();
+				if (movieName.indexOf(listMovieName[indexMovieName]) > 0 && !lista.includes(pListMovies[indexMovie])){
+					lista.push(pListMovies[indexMovie]);
+				}	
+			}
+		}
+		return lista;
+	}	
+
+	function searchMovieCast(pListMovies, pActor, lista1){
+		for (var indexMovie in pListMovies) {		
+			listMovieCast = commaSplit(pActor);		
+			for (indexMovieCast in listMovieCast)
+			{
+				if (pListMovies[indexMovie].cast != null){
+					movieCast = pListMovies[indexMovie].cast.toLowerCase();
+					if (movieCast.indexOf(listMovieCast[indexMovieCast]) > 0 && !lista1.includes(pListMovies[indexMovie])){
+						lista1.push(pListMovies[indexMovie]);
+					}
+				}	
+			}
+		}
+		return lista1;
+	}
+
+	function HashTable(obj)
+	{
+		this.length = 0;
+		this.items = {};
+		for (var p in obj) {
+		    if (obj.hasOwnProperty(p)) {
+		        this.items[p] = obj[p];
+		        this.length++;
+		    }
+		}
+
+
+		this.setItem = function(key, value)
+		{
+			var listMovies = [];
+		    var previous = undefined;
+		    if (this.hasItem(key)) {
+		        previous = this.items[key];
+		    }
+		    else {
+		    	this.items[key] = listMovies;
+		        this.length++;
+		    }
+		    this.items[key].push(value);
+		    return previous;
+		}
+
+		this.hasItem = function(key)
+		{
+		    return this.items.hasOwnProperty(key);
+		}
+
+		this.setItemYear = function(key, value, pMovie)
+		{
+			var genreHash = new HashTable();
+			var previous = undefined;
+		    if (this.hasItemYear(key)) 
+		    {
+		        previous = this.items[key];
+		    }
+		    else 
+		    {
+		    	this.items[key] = genreHash;
+		        this.length++;
+		    }
+		   	this.items[key].setItem(value, pMovie);
+		    return previous;
+		}
+
+		this.getItem = function(key) {
+		    return this.hasItem(key) ? this.items[key] : undefined;
+		}
+
+		this.getItemYears = function(key) {
+		    return this.hasItemYear(key) ? this.items[key] : undefined;
+		}
+
+		this.hasItemYear = function(key)
+		{
+		    return this.items.hasOwnProperty(key);
+		}
+
 	}
 
 }).listen (8080);﻿
